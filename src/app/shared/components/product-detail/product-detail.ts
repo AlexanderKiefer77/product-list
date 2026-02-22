26305eba2dd97fdcd25c4ae7fb0ea6ccbd91b3d1
Alexander Kiefer <Alexander.Kiefer77@protonmail.com>
2026-02-22 16:36:16 +0100

    modified product-detail with variables

--- src/app/shared/components/product-detail/product-detail.ts
+++ src/app/shared/components/product-detail/product-detail.ts
@@ -8,10 +8,14 @@ import { Component } from '@angular/core';
 })
 export class ProductDetail {
   detail = {
-      "name": "Gaming Maus",
-      "description": "Eine ergonomische Gaming-Maus mit hoher Präzision und einstellbarer DPI.Ideal für FPS- und MOBA - Spiele, bietet sie eine langlebige Bauweise und komfortable Seitentasten für schnelles Reagieren.",
-      "specs": "dpi: 6400, cable length: 1.8m, color: Schwarz",
-      "stock": 120,
-      "price": 29.99
-    }
+    "name": "Gaming Maus",
+    "description": "Eine ergonomische Gaming-Maus mit hoher Präzision und einstellbarer DPI.Ideal für FPS- und MOBA - Spiele, bietet sie eine langlebige Bauweise und komfortable Seitentasten für schnelles Reagieren.",
+    "specs": "dpi: 6400, cable length: 1.8m, color: Schwarz",
+    "stock": 120,
+    "price": 29.99
+  }
+
+  deleteDetail() {
+    this.detail.name = "";
+  }
 }
